0e2b5a1a15d627ce6cfa9280d636f61a90a6b963
eleanablandin <eleanablandin@gmail.com>
2021-06-10 15:46:38 -0400

    database creada con Psql, problemas para la compretar la migracion y hacer upgrade

--- src/models.py
+++ src/models.py
@@ -1,15 +1,56 @@
+import  os
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Agent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.String(16), unique=True, nullable=False)
+    salt = db.Column(db.String(40), nullable=False)
+    hashed_password = db.Column(db.String(240), nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, **kwargs):
+        print(kwargs)
+        self.email = kwargs.get('email')
+        self.name = kwargs.get('name')
+        self.last_name = kwargs.get('last_name')
+        self.phone = kwargs.get('phone')
+        self.salt = os.urandom(16).hex()
+        self.set_password(kwargs.get('password')
+
+    @classmethod
+    def create(cls, **kwargs):
+        agent = cls(**kwargs)
+        db.session.add(agent)
+        try: 
+            db.session.commit()
+        except Exception as error:
+            print(error.args)
+            db.session.rollback()
+            return False
+        return agent 
+
+    def set_password(self, password):
+        print(generate_password_hash(
+            f"{password}{self.salt}"
+        ))
+        self.hashed_password = generate_password_hash(
+            f"{password}{self.salt}"
+        )
+
+    def check_password(self, password):
+        return check_password_hash(
+            self.hashed_password,
+            f"{password}{self.salt}"
+        )   
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Agent %r>' % self.username
 
     def serialize(self):
         return {
